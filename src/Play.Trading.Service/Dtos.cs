94ab562a62ff76787a6c7b8b80e4ec3e7718ae60
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-04-13 20:39:56 +0300

    add states for state machine and a purchase controller to publish the message

---
+++ src/Play.Trading.Service/Dtos.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Trading.Service
+{
+    public record SubmitPurchaseDto(
+        [Required]Guid? ItemId,
+        [Range(1, 100)]int Quantity
+    );
+
+}
